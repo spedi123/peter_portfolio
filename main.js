7532cc5337f609c47f4811ce0ffa7d4df0249956
spedi123 <peteran8604@gmail.com>
2021-11-01 14:01:38 -0700

    javascript project

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
   } else {
@@ -50,6 +48,26 @@ document.addEventListener("scroll", () => {
 // Handle clike on the "arrow up"
 arrowUp.addEventListener("click", () => {
   scrollIntoView("#home");
+});
+
+// Projects
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  projects.forEach((project) => {
+    if (filter === "*" || filter === project.dataset.type) {
+      project.classList.remove("invisible");
+    } else {
+      project.classList.add("invisible");
+    }
+  });
+  console.log(filter);
 });
 
 function scrollIntoView(selector) {
